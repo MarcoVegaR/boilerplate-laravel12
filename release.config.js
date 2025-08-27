1d00cea466b090649a50ee6f808c72f27c204776
MarcoVegaR <marcovega11@gmail.com>
2025-08-26 22:46:11 -0400

    fix(release): remove changelog notes from commit to comply with 100-char limit

--- release.config.js
+++ release.config.js
@@ -8,7 +8,7 @@ export default {
             '@semantic-release/git',
             {
                 assets: ['CHANGELOG.md'],
-                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+                message: 'chore(release): ${nextRelease.version} [skip ci]',
             },
         ],
         '@semantic-release/github',
